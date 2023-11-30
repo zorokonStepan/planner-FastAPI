5e705c6ce440ab5c0ac10c7c5a41d22aa2d48689
SKolodkin <dev1@instrument-fit.ru>
2023-11-30 08:42:19 +0300

    create models

---
+++ models/users.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+
+
+class NewUser(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
