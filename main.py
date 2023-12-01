caa0595d8591dd136e2d5f93b82d84b8801c7577
SKolodkin <dev1@instrument-fit.ru>
2023-12-01 08:28:50 +0300

    create event routes

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from routes.users import user_router
+from routes.events import event_router
 
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 
 if __name__ == "__main__":
