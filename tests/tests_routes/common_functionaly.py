0ae4dee350d774d52af057ceffdfa8c1afb961c2
SKolodkin <dev1@instrument-fit.ru>
2023-12-01 08:41:09 +0300

    create tests_routes

---
+++ tests/tests_routes/common_functionaly.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class Requester:
+    RESPONSE_TIMEOUT = 5
+
+    @classmethod
+    def __request(cls, url, method="GET", json_data=None):
+        try:
+            response = requests.request(method,
+                                        url,
+                                        headers={"Content-Type": "application/json"},
+                                        timeout=cls.RESPONSE_TIMEOUT,
+                                        json=json_data)
+
+            assert response.status_code == 200
+            return response.json()
+
+        except AssertionError as exception:
+            print(f'{exception = }')
+        except Exception as exception:
+            print(f'{exception = }')
---
+++ tests/tests_routes/test_events.py
@@ -0,0 +1,4 @@
+
+
+class TestEventsRoutes:
+    pass
